783a3fc6925e0fd4732cec47951860b0dee94184
boukew99 <boukewestra0@gmail.com>
2022-07-26 00:20:21 +0200

    chess update, clipboard

--- chess/chess.js
+++ chess/chess.js
@@ -1,42 +1,56 @@
 //https://www.w3docs.com/snippets/javascript/how-to-get-url-parameters.html
 //https://stackoverflow.com/questions/3065342/how-do-i-iterate-through-table-rows-and-cells-in-javascript
 
-window.onload = function setupDragAndDrop() {
-		
-		document.ondragstart = function(event) {
-			event.dataTransfer.setData("cell", event.target.innerHTML)
-		}
-		
-		document.ondrop = function(event){
-    	  event.target.innerHTML = event.dataTransfer.getData("cell")
-    	  event.target.draggable = true
-		}
-  
-    document.ondragover = function(event){
-			event.preventDefault()
-    }
-    
-	const cells = document.getElementsByTagName("td") 
-  
+window.onload = function(event) {
 	const urlParams = new URLSearchParams(window.location.search);
 	const setup = urlParams.get('setup');
 	
-  for (let i = 0; i < cells.length; i++) {  
-		cell = cells[i]
-		cell.innerHTML = setup[i]
-		cell.setAttribute("draggable", cell.innerHTML != "")
-
-  }
-  
-	
-	var table = document.getElementById("chessboard");
-	for (var i = 0, cell; cell = table.cells[i]; i++) {
-		cell.innerHTML = pos[i]
-		alert(pos[i])
+	if (setup) {
+		const cells = document.getElementsByTagName("td") 
 		
-}
+		for (let i = 0, cell;cell = cells[i]; i++) {  
+			cell.innerHTML = (setup[i]) ? setup[i] : "";
+			cell.setAttribute("draggable", cell.innerHTML != "")
+		}
+		
+	}
+	
 }
 
+document.ondragstart = function(event) {
+	event.dataTransfer.setData("cell", event.target.id)
+}
+
+document.ondrop = function(event){
+	event.preventDefault()
+	from = event.dataTransfer.getData("cell")
+	var cell = document.getElementById( from )
+	
+	if	( validate_move(cell.innerHTML, from, event.target.id) ) return
+	
+	event.target.innerHTML = cell.innerHTML
+	event.target.draggable = true
+	cell.innerHTML = ""
+	cell.draggable = false
+	
+}
+
+document.ondragover = function(event){
+	event.preventDefault()
+}
+
+
+function validate_move(piece, from, to) {
+	switch (piece) {
+		case "♜", "♖", "n", "N":
+			break
+		case "♟", "P":
+		  alert("pawns can only move forward and capture diagonally.")
+		  return true
+		case "♙", "p":
+			break
+	}
+}
 
 
 function flip_fen(fen){
--- chess/setups.md
+++ chess/setups.md
@@ -5,7 +5,7 @@
 
 Name | Difficulty | Setup
 ---  |---         |---
-Inversed | X | [rp....PR/np....PN/bp....PB/qp....PQ/kp....PK/bp....PB/np....PN/rp....PR](chess?setup=rp4PR/np4PN/bp4PB/qp4PQ/kp4PK/bp4PB/np4PN/rp4PR)
-Inversed (mirror) | X | [rp\np\bp\qp\kp\bp\np\rp\](chess?setup=rp4PR/np4PN/bp4PB/qp4PQ/kp4PK/bp4PB/np4PN/rp4PR)
+Inversed | X | [rp....PR/np....PN/bp....PB/qp....PQ/kp....PK/bp....PB/np....PN/rp....PR](?setup=rp4PR/np4PN/bp4PB/qp4PQ/kp4PK/bp4PB/np4PN/rp4PR)
+Inversed (mirror) | X | [rp\np\bp\qp\kp\bp\np\rp\](?setup=rp4PR/np4PN/bp4PB/qp4PQ/kp4PK/bp4PB/np4PN/rp4PR)
 Working | X | [♖♙____♟♜♘♙____♟♞♗♙____♟♝♕♙____♟♛♔♙____♟♚♗♙____♟♝♘♙____♟♞♖♙____♟♜](chess?setup=♖♙____♟♜♘♙____♟♞♗♙____♟♝♕♙____♟♛♔♙____♟♚♗♙____♟♝♘♙____♟♞♖♙____♟♜)
-Dance of Kings | X | [k6K](chess?setup=k6K)
+Dance of Kings | X | [k6K](?setup=k6K)
