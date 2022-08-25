4380f621416aef1f153219af8b0015885655f763
boukew99 <boukewestra0@gmail.com>
2022-08-26 00:06:06 +0200

    sudoku

---
+++ sudoku/script.js
@@ -0,0 +1,10 @@
+window.onload = function(e) {
+		const cells = document.getElementsByTagName("td") 
+		const temp = document.getElementsByTagName("template")[0];
+		
+		for (let i = 0; i < 9*9;i+=1) {
+			var cell = cells[i];
+			var clon = temp.content.cloneNode(true);
+			cell.appendChild(clon);
+		}
+}
